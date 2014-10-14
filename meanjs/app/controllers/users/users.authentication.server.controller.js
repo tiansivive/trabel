14b177aa48e6cf74aa99df68d3fceb1ada77e6b2
vascoFG <vascofg@gmail.com>
2014-10-14 17:52:03 +0100

    Removed log message

--- meanjs/app/controllers/users/users.authentication.server.controller.js
+++ meanjs/app/controllers/users/users.authentication.server.controller.js
@@ -28,7 +28,6 @@ exports.signup = function(req, res) {
 	// Then save the user
 	user.save(function(err) {
 		if (err) {
-			console.log('AQUI', user);
 			return res.status(400).send({
 				message: errorHandler.getErrorMessage(err)
 			});
