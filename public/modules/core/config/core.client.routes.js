ff71309204a6923c3e60cbd28cb1b90be5f37060
João Almeida <alm3id4@gmail.com>
2014-12-10 18:21:14 +0000

    Created Dashboard

--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -16,6 +16,10 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 		state('home', {
 			url: '/',
 			templateUrl: 'modules/core/views/home.client.view.html'
+		}).
+    	state('dash', {
+			url: '/dashboard',
+			templateUrl: 'modules/core/views/dashboard.client.view.html'
 		});
 	}
 ]);
